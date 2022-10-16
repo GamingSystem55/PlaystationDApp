673d7463c51f6e9dd4756366c29c36b3003629a0
EugeneMaz10 <eugenengaira863@gmail.com>
2022-10-16 22:06:33 +0300

    ejs uploader

--- js/app.js
+++
@@ -1,7 +0,0 @@
-
-const ipfsClient = require('ipfs-http-client');
-const express = require('express');
-const bodyParser = require('body-parser');
-const fileUpload = require('express-fileupload');
-
-const ipfs = new ipfsClient({host:'localhost',port})
